d0110570719114e66bc5861ac0ea51aeffbf927d
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-04 05:05:28 +0100

    Built out structure of Game component and added SVG card deck

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react'
 import shuffle from '../../utils/shuffle'
 
 export default class Game extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            cards: '',
+            matched: [],
+            selected: []
+        }
+    }
+
+    selectHandler = (index) => {
+
+    }
+
     render() {
+        const { cards, matched, selected } = this.state;
+
         return (
             <div>
                 <p>Game</p>

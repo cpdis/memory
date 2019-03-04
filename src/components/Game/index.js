bbf6cd91758ffbe2d6a91e91491c4f102b99a291
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-04 19:17:06 +0100

    Created first iteration of card selectHandler

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -14,35 +14,35 @@ export default class Game extends Component {
     }
 
     selectHandler = (index) => {
-        const { cards, matched, selected } = this.state;
+        const { cards, matched, selected } = this.state
 
-        if (selected.length === 0) {
+        if (selected.length === 0) { // Select the first card
             this.setState({ selected: [index] })
-        } else if (selected.length === 1) {
-            // if () {
-            //     // The cards are a match
-            //     this.setState({})
-            // } else {
-            //     // The cards are not a match
-            //     this.setState({})
-            // }
+        } else if (selected.length === 1) { // Select the second card
+            if (cards[selected[0]].value === cards[index].value &&  // Check if card value is matching
+                cards[selected[0]].suit === cards[index].suit) {   // TODO: Check if card color is matching (dependent on suit)
+                // The cards are a match
+                this.setState({
+                    // TODO: Added code to remove cards from deck when matched
+                    matched: matched.concat([selected[0], index]),
+                    selected: []
+                })
+            } else {
+                // The cards are not a match
+                this.setState({
+                    selected: [selected[0], index]
+                })
+            }
         }
     }
 
     render() {
-        const { cards, matched, selected } = this.state;
+        const { cards, matched, selected } = this.state
 
         return (
             <div>
                 <h1>Memory</h1>
                 <p>Memory (also known as Concentration) is a card game played with one or more players. Using a standard card deck (including both jokers) the players shuffle the deck and lay all of the cards face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards. Concentration can be played with any number of players or as solitaire.</p>
-                {this.state.cards.map(card => (
-                    <div>
-                        <li>
-                            {card.id}, {card.suit}, {card.value}
-                        </li>
-                    </div>
-                ))}
             </div>
         )
     }

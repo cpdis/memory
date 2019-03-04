97942c99ce9f19c0578d73fe2f776a8221923924
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-04 04:57:08 +0100

    Created folder structure and skeleton components and wrote random util function

---
+++ src/components/Game/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import shuffle from '../../utils/shuffle'
+
+export default class Game extends Component {
+    render() {
+        return (
+            <div>
+                <p>Game</p>
+            </div>
+        )
+    }
+}

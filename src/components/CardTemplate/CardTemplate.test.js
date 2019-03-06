511f581c02d9b08e64944b4d03ef0864fe8fd01e
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-06 04:33:51 +0100

    Added test to render CardTemplate component

--- src/components/CardTemplate/CardTemplate.test.js
+++ src/components/CardTemplate/CardTemplate.test.js
@@ -6,6 +6,16 @@ import "jest-dom/extend-expect";
 import CardTemplate from './index'
 
 describe('CARD', () => {
+    const card = {
+        id: 1,
+        suit: 'hearts',
+        value: 2,
+        img: require('../../data/SVG_Cards/2_of_hearts.svg')
+    }
+
     it("Render CardTemplate component", () => {
+        const div = document.createElement("div")
+        ReactDOM.render(<CardTemplate card={card} />, div)
+        ReactDOM.unmountComponentAtNode(div)
     })
 })
